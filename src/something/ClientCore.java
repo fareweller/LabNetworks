9e4e36a5ed25ca4122ae2b063ea28b9dbf7efc0a
Fareweller <stalker20210@yandx.ru>
2015-09-15 03:18:36 +0300

    Worked some out. Made sendMessage and recieveMessage methods. Also made a tiny main.  It's a bit, but that's a start.

--- src/something/ClientCore.java
+++ src/something/ClientCore.java
@@ -1,7 +1,34 @@
 package something;
 
-/**
- * Created by stalker20210 on 14.09.2015.
- */
-public class ClientCore {
+import java.io.*;
+import java.net.*;
+
+ public class ClientCore {
+
+    public enum msg {connect, msg, disconnect}
+
+    public void sendMessage(msg type, String message,DatagramSocket clientSocket, InetAddress IPAddress) throws Exception{
+        byte[] sendData = message.getBytes();
+        DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9876);
+        clientSocket.send(sendPacket);
+    }
+    public String recieveMessage(DatagramSocket clientSocket)throws Exception{
+        byte[] receiveData = new byte[1024];
+        DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+        clientSocket.receive(receivePacket);
+        return new String(receivePacket.getData());
+
+    }
+
+    public static void main(String args[]) throws Exception {
+        ClientCore messenger = new ClientCore();
+        BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+        DatagramSocket clientSocket = new DatagramSocket();
+        InetAddress IPAddress = InetAddress.getByName("127.0.0.1");
+        String sentence = inFromUser.readLine();
+        messenger.sendMessage(msg.connect, sentence, clientSocket, IPAddress);
+        String recievedSentence = messenger.recieveMessage(clientSocket);
+        System.out.println("FROM SERVER:" + recievedSentence);
+        clientSocket.close();
+    }
 }
