65bf28c983849da599abb8364a482f72bb0256da
6aJIaJIae4HuK <timuratshin@yandex.ru>
2015-10-27 20:45:32 +0300

    New ClientGui (with scale and more readable code). Added initial ban-unban functions to Server class.

--- src/ru/com/miet/mp45/NetworkProject/Lab1/ClientGUI/Controller.java
+++ src/ru/com/miet/mp45/NetworkProject/Lab1/ClientGUI/Controller.java
@@ -1,5 +1,6 @@
 package ru.com.miet.mp45.NetworkProject.Lab1.ClientGUI;
 
+import com.sun.nio.sctp.MessageInfo;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.control.Button;
@@ -37,13 +38,23 @@ public class Controller {
     private Button connectToServerButton;
 
     @FXML
+    private Button disconnectFromServerButton;
+
+    @FXML
     private Button sendMessageButton;
 
     @FXML
     private void connectToServer() {
-        String name = nickNameField.getText();
-        String serverIP = serverIPField.getText();
-        String serverPort = serverPortField.getText();
+        String name = nickNameField.getText().trim();
+        String serverIP = serverIPField.getText().trim();
+        String serverPort = serverPortField.getText().trim();
+        if (name.equals("") || serverIP.equals("") || serverPort.equals(""))
+        {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setContentText("Не все поля заполнены!");
+            alert.showAndWait();
+            return;
+        }
         try {
             int p = Integer.parseInt(serverPort);
             app.getClient().connectToServer(InetAddress.getByName(serverIP), p, name);
@@ -53,6 +64,16 @@ public class Controller {
         }
         catch (UnknownHostException e) {
             e.printStackTrace();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void disconnectFromServer() {
+        try {
+            app.getClient().disconnectFromServer();
         }
         catch (IOException e) {
             e.printStackTrace();
@@ -75,5 +96,7 @@ public class Controller {
         clientsListView.setItems(app.getConnectedClients());
         messagesListView.setItems(app.getReceivedMessages());
         sendMessageButton.disableProperty().bindBidirectional(app.getClient().isNotConnected());
+        connectToServerButton.disableProperty().bindBidirectional(app.getClient().isConnected());
+        disconnectFromServerButton.disableProperty().bindBidirectional(app.getClient().isNotConnected());
     }
 }
